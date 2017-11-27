8977baf8c28d107d4766187892cf8bbb14d2ef3f
Jacob Bates <jacobbates@users.noreply.github.com>
2017-11-27 20:38:32 +1100

    Initial Upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Coinspot++ Google Chrome Extension
+
+Coinspot++ is an unofficial Chrome extension for [Coinspot Australia](https://coinspot.com.au) BUY & SELL pages that adds enhanced filtering of coins to the website. This extension adds additional accessibility to Coinspot by adding pagination, sortable columns and quick search functionality.
+
+This extension is provided FREE, requires no permissions to be accepted in Chrome and is Open Sourced making code available for your inspection.
+
+![Coinspot++ Screenshot](https://lh3.googleusercontent.com/j89VXVr8fA3FM6aqX_6q5H5BIIpi4Sslws7wtb5Tl-G-Pn5qJav3jMEBY0JJyPQaH13vLkYuhA=w640-h400-e365)
+
+Coinspot++ is not affiliated with Coinspot or coinspot.com.au.
+
+## Getting Started
+
+Coinspot++ is [available to install via the Chrome Web Store](https://chrome.google.com/webstore/detail/coinspot%20%20/kkhjbgfpkliinnoaibhkcifeddpechfc). Once enabled, simply browse to the coinspot.com.ay [buy page](https://www.coinspot.com.au/tradecoins) or [sell page](https://www.coinspot.com.au/sellallcoins) to view it in action.
+
+We recommend bookmarking these pages in Chrome if you frequent them often. Advanced users can clone this repository and run a local version of the extension in developer mode.
+
+### Features
+
+Coinspot++ will currently ONLY effect the following pages:
+* https://www.coinspot.com.au/tradecoins
+* https://www.coinspot.com.au/sellallcoins
+
+```
+* Converts BUY/SELL listings into a **Data Table**
+* Adds **Pagination** to reduce page size | 20, 50 or 100 coins per page.
+* Adds **Quick Search** input for finding specific coins or data
+* Adds **Sortable Columns** to Data Table for filtering by Coin, Buy Rate, Market Cap and 24-hour Change (default is Market Cap).
+```
+
+## Built With
+
+* [Bootstrap v3.1.1](http://getbootstrap.com)
+* [Jquery 3.2.1](https://jquery.com/)
+* [DataTables 1.10.16](https://datatables.net/)
+
+## Author
+
+Extension created by Jacob Bates (https://github.com/jacobbates)
+
+Find this project useful? Feel free to buy me a beer =)
+
+Donation Address: [1KmdDF2QtBsacEPtqEi3vXqciJjcmiCS36](https://blockchain.info/address/1KmdDF2QtBsacEPtqEi3vXqciJjcmiCS36)
+
+## License
+
+This project is licensed under the GNU AGPLv3 License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ content.js
@@ -0,0 +1,144 @@
+	/*
+	Coinspot++ Google Chrome Extension
+	Version: 1.0.2
+	Unofficial coinspot.com.au sorting enhancement for BUY/SELL pages
+	Author: Jacob A Bates (https://github.com/jacobbates)
+	License: GNU AGPLv3 (https://www.gnu.org/licenses/agpl-3.0.txt)
+	
+	Coinspot++ is not affiliated with Coinspot or coinspot.com.au
+	
+	Credits:
+	Bootstrap v3.1.1 © 2011-2014 Twitter, Inc. | https://github.com/twbs/bootstrap/blob/master/LICENSE
+	jQuery v3.2.1 | (c) JS Foundation and other contributors | jquery.org/license
+	DataTables 1.10.16 ©2008-2017 SpryMedia Ltd | datatables.net/license
+	DataTables Bootstrap 3 integration ©2011-2015 SpryMedia Ltd | datatables.net/license
+	*/
+
+	/*Function: isOdd
+	  Boolean for odd number
+	  Coinspot alternates rows for desktop and mobile
+	  This is used to check and only append desktop rows to the DOM*/
+	function isOdd(num) {
+		return num % 2;
+	}
+
+	/*Function: CoinTable
+	  Where the magic happens
+	  Rewrites <ul> and <li> into table rows
+	  Also adds bootstrap visibility classes to specific columns hidden on mobile */
+	jQuery.fn.CoinTable = function() {
+		return this.each(function() {
+
+			var table = $('<table id="CoinTable">');
+			var thead = $('<thead>');
+			var tbody = $('<tbody>');
+
+			$(this).children('li').each(function(i, li){
+
+				var coin = $(this).data('coin');
+
+				if (i == 0||i == 1){
+
+					//Table Head
+					$(li).children("div").each(function(ii, row){
+						var tr = $('<tr>');
+						$(row).children("div").each(function(iii, col){
+							var th = $('<th>');
+							if(iii!==4){
+								if(iii==2||iii==5){
+									th.append($(this).html()).addClass('hidden-xs hidden-sm');
+								}else{
+									colhtml = $(this).html()
+									if(iii==1){
+									    var str = colhtml.split(" ");
+									    colhtml = str[0]+'<span class="hidden-xs hidden-sm"> '+str[1]+'</span>';
+									}
+									if(iii==3){
+									    var str = colhtml.split(" ");
+									    colhtml = str[0]+'<span class="hidden-xs hidden-sm"> '+str[1]+'</span>';
+									}
+									th.append(colhtml);
+								}
+								tr.append(th).addClass('row-classss');
+							}
+						});
+						if (!isOdd(i)) {thead.append(tr);}
+					});
+					table.append(thead);
+
+				} else {
+
+					//Table Body
+					$(li).children("div").each(function(ii, row){
+						var tr = $('<tr>');
+						$(row).children("div").each(function(iii, col){
+							var td = $('<td>');
+							if(iii!==4){
+								if(iii==2||iii==5){
+									td.append($(this).html()).addClass('hidden-xs hidden-sm');
+								}else{
+									colhtml = $(this).html()
+									if(iii==0){
+									    var str = colhtml.split("&nbsp;");
+									    colhtml = str[0]+'<span class="hidden-xs hidden-sm">'+str[1]+'</span><span class="hidden-md hidden-lg hidden-xl">'+coin+'</span>';
+									}
+									td.append(colhtml);
+								}
+								tr.append(td).addClass('classss');
+							}
+							
+						});
+						if (!isOdd(i)) {tbody.append(tr.attr("data-coin", coin));}
+					});
+				}
+			});
+			$(this).after(table.append(tbody).addClass('table table-hover table-responsive')).remove();
+		});
+	}
+
+	//JS to execute on Page Load
+	$(document).ready(function() {
+
+		//Removes Large 3-Col Panel moving coins up page 
+		$('.panel').parent().remove();
+
+		//Converts Coinspot list to Table
+		$('ul.listgroup').CoinTable();
+
+		//Initiate Sortable DataTable
+		$('#CoinTable').DataTable({
+			"order": [[2, 'dsc']],
+			"autoWidth": false,
+			"pageLength": 20,
+			"lengthMenu": [20, 50, 100],
+			"language": {
+				"lengthMenu": "<div class='input-group'><span class='input-group-addon'><i class='glyphicon glyphicon-th-list'></i></span>_MENU_</div>",
+				"info": "Displaying _START_-_END_ of _TOTAL_",
+            	"infoFiltered": "(Filtered)",
+            	"infoEmpty": "Displaying 0 to 0 of 0",
+            	"search": "<div class='input-group'><span class='input-group-addon'><i class='glyphicon glyphicon-search'></i></span>_INPUT_</div>",
+            	"paginate": {
+            		"next": ">",
+            		"previous": "<"
+            	},
+			}
+		});
+
+		//Adds clickable hyperlinks to rows
+		$('#CoinTable tbody').on('click', 'tr', function () {
+        	var coin = $(this).data("coin");
+        	//Check if Buy or Sell page using H1
+        	if($('h1').html()=="Buy Coins"){
+        		window.location = "/buy/"+coin;
+        	} else if ($('h1').html()=="Sell Coins"){
+				window.location = "/sell/"+coin;
+        	}
+    	} );
+
+    	//Update navbar branding on BUY/SELL list pages to Coinspot++
+    	//Avoids confusion about which pages extension effects
+    	var branding = $('.navbar-brand').html();
+    	$('.navbar-brand').html(branding+"++");
+
+	});
+
